a3475c1319840fbb21cc4faa8be482e30992a5f1
MD Munjurul islam <mdmunjurul319@gmail.com>
2026-04-03 23:24:16 +0600

    Add programs route with CRUD operations

---
+++ routes/programs.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { getAllPrograms, getProgramsByUniversity, createProgram, updateProgram, deleteProgram } = require('../controllers/programController');
+const { authenticateUser, requireAdmin } = require('../middleware/auth');
+
+router.get('/', getAllPrograms);
+router.get('/university/:university_id', getProgramsByUniversity);
+router.post('/', authenticateUser, requireAdmin, createProgram);
+router.put('/:id', authenticateUser, requireAdmin, updateProgram);
+router.delete('/:id', authenticateUser, requireAdmin, deleteProgram);
+
+module.exports = router;
